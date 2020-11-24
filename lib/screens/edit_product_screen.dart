ea294d4d11ba94fb47c7fa169a486be63bef3026
arsh <arsh@ubxty.com>
2020-11-24 11:30:27 +0530

    MultiLine Input & Disposing Objects

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -9,6 +9,14 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode(); // for showing the focus on the prce
+  final _descriptionFocusNode = FocusNode(); // for showing focus on description
+
+  @override
+  void dispose() {
+    super.dispose();
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +45,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next, // means after enter go to the next input not submit the whole form
                 keyboardType: TextInputType.number, // for showing the number keyboard
                 focusNode: _priceFocusNode, // to set focuse by othernode we use the focus node
+                onFieldSubmitted: (value) { // on field submitted we will set focus to the price focus node
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode); // when submitted then show focus on description node
+                },
+              ),
+              TextFormField( // connected with form behind the scene
+                decoration: InputDecoration( // to showing the decoration
+                  labelText: 'Description', // showing the label inside dummy data
+                ),
+                maxLines: 3, // for setup max amount of line user can fill
+                keyboardType: TextInputType.multiline, // for supporting multiline
+                focusNode: _descriptionFocusNode,
               ),
             ],
           ),
