d2556930bed1416755460757f93a1d2ae2092f7f
arsh <arsh@ubxty.com>
2020-11-24 11:14:04 +0530

    Managing Focus

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -8,6 +8,8 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode(); // for showing the focus on the prce
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +26,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   labelText: 'Title', // showing the label inside dummy data
                 ),
                 textInputAction: TextInputAction.next, // means after enter go to the next input not submit the whole form
+                onFieldSubmitted: (value) { // on field submitted we will set focus to the price focus node
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField( // connected with form behind the scene
+                decoration: InputDecoration( // to showing the decoration
+                  labelText: 'Price', // showing the label inside dummy data
+                ),
+                textInputAction: TextInputAction.next, // means after enter go to the next input not submit the whole form
+                keyboardType: TextInputType.number, // for showing the number keyboard
+                focusNode: _priceFocusNode, // to set focuse by othernode we use the focus node
               ),
             ],
           ),
