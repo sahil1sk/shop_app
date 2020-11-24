6ba77fb101b2cba7f67daf852ab0ee6e9c4c293a
arsh <arsh@ubxty.com>
2020-11-24 11:04:02 +0530

    Using Forms & Working with Form

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+
   @override
   _EditProductScreenState createState() => _EditProductScreenState();
 }
@@ -12,7 +14,21 @@ class _EditProductScreenState extends State<EditProductScreen> {
       appBar: AppBar(
         title: Text('Edit Product'),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField( // connected with form behind the scene
+                decoration: InputDecoration( // to showing the decoration
+                  labelText: 'Title', // showing the label inside dummy data
+                ),
+                textInputAction: TextInputAction.next, // means after enter go to the next input not submit the whole form
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
