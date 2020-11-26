efac90b4facf2e2cb3cf5ddd829da609ad874187
sahil1sk <khannasahil303@gmail.com>
2020-11-26 07:45:28 +0530

    Pull to Refresh

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -10,6 +10,10 @@ import 'edit_product_screen.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -26,20 +30,23 @@ class UserProductsScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemBuilder: (ctx, index) => Column(
-            children: <Widget>[
-              UserProductItem(
-                productsData.items[index].id,
-                productsData.items[index].title, 
-                productsData.items[index].imageUrl,
-              ),
-              Divider(), // horizontal line
-            ],
+      body: RefreshIndicator( // helps to show refresh icon on pull
+        onRefresh: () => _refreshProducts(context), // the function we pass will return a future menas when the indicator will be removed from the screen
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemBuilder: (ctx, index) => Column(
+              children: <Widget>[
+                UserProductItem(
+                  productsData.items[index].id,
+                  productsData.items[index].title, 
+                  productsData.items[index].imageUrl,
+                ),
+                Divider(), // horizontal line
+              ],
+            ),
+            itemCount: productsData.items.length
           ),
-          itemCount: productsData.items.length
         ),
       )
     );
