c9107db395dc715ed00f6e691324784ee25ad0f3
sahil1sk <khannasahil303@gmail.com>
2020-11-26 15:08:36 +0530

    Future Builder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,60 +5,47 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
-    print(orderData.orders.length);
+    print('building orders');
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : orderData.orders.length > 0
-              ? ListView.builder(
-                  itemBuilder: (ctx, index) =>
-                      OrderItem(orderData.orders[index]),
+      body: FutureBuilder(
+        // Using future builder makes easy
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        // in future element set the data which is return in the future
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            // means connection is in progress right now
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              // ...
+              // Do error handling stuff
+              return Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              // Here we use Consumer instead of Provider
+              return Consumer<Orders>(
+                // if we use consumer instead of provider then the provider will notify by the Orders and then this widget will built again
+                builder: (ctx, orderData, child) => ListView.builder(
+                  // child element we will use then to place any where because it will not be built again
                   itemCount: orderData.orders.length,
-                )
-              : Container(
-                  padding: EdgeInsets.symmetric(vertical: 5),
-                  alignment: Alignment.topCenter,
-                  child: Text(
-                    "You didn't place any order yet.",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
                 ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
