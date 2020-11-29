114f5fd422b7d9b299fcec35f6dba54948a93060
sahil1sk <khannasahil303@gmail.com>
2020-11-29 11:29:39 +0530

    Sliver AppBar

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -15,16 +15,17 @@ class ProductDetailScreen extends StatelessWidget {
       ).findById(productId);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero( // giving the float in effect
+      // appBar: AppBar(
+      //   title: Text(loadedProduct.title),
+      // ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 300, //total height taken by appbar
+            pinned: true, // appbar is always visible if scroll it set to the top
+            flexibleSpace: FlexibleSpaceBar( //flexibleSpace what is inside of the appbar
+              title: Text(loadedProduct.title), // appbar title
+              background: Hero( // giving the float in effect // background is part that always not visible it is only visible when the app is expanded
                 tag: loadedProduct.id, // unique tag
                 child: Image.network(
                   loadedProduct.imageUrl,
@@ -32,26 +33,34 @@ class ProductDetailScreen extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 10), // for spacing
-            Text(
-              '\$${loadedProduct.price}',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              width: double.infinity,
-              child: Text(
-                loadedProduct.description,
-                textAlign: TextAlign.center,
-                softWrap: true,
-              ),
-            )
-          ],
-        ),
+          ),
+          SliverList( // Things which will show after the upper appbar
+            delegate: SliverChildListDelegate(
+              [
+                SizedBox(height: 10), // for spacing
+                Text(
+                  '\$${loadedProduct.price}',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  width: double.infinity,
+                  child: Text(
+                    loadedProduct.description,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                  ),
+                ),
+                SizedBox(height: 800),
+              ],
+            ), //delegate helps how to render content on screen
+          ),
+        ],
       ),
     );
   }
